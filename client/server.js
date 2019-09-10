3fb75ea3b654f7440012e6c8ff67362c1687ffbb
Neztore <josh@muir.xyz>
2019-09-10 22:12:54 +0100

    Client work

--- client/server.js
+++ client/server.js
@@ -1,9 +1,22 @@
 const express = require("express")
+const path = require("path")
+
 const app = express()
 const port = 80
 
-app.use(express.static(''))
+const srcPath = path.join(__dirname, "src")
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const jsPath = path.join(srcPath, "js")
+const cssPath = path.join(srcPath, "css")
+const imgPath = path.join(srcPath, "img")
+const pagePath = path.join(srcPath, "pages")
+
+app.use('/js', express.static(jsPath))
+app.use('/css', express.static(cssPath))
+app.use('/img', express.static(imgPath))
+
+app.get('/', (req, res) =>
+res.sendFile(path.join(pagePath, "index.html"))
+)
 
 app.listen(port, () => console.log(`Client listening: port ${port}!`))
