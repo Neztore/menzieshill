97e4f0bc749651c3efde7ff65c44cf5771fc3538
Josh Muir <josh@muir.xyz>
2020-04-20 16:00:07 +0100

    Add configs (Minus sensitive info)

---
+++ api/config.js
@@ -0,0 +1,19 @@
+// Will later take env variables and provide them to app.
+const path = require("path");
+const {
+  port = 3000,
+  authLife = 604800,
+  filesLoc = path.join(__dirname, "files"), // where files are stored
+  recaptchaToken,
+  sentryDsn,
+  emailToken
+} = process.env
+
+module.exports = {
+  port,
+  authLife,
+  filesLoc,
+  recaptchaToken,
+  sentryDsn,
+  emailToken
+}
